5dea30b2ecbb3990467cd2fb66c2e83b33647250
David Diaz <david.diaz@zweicom.com>
2018-12-03 12:36:39 -0300

    se agrega url

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ export default class Footer extends React.Component{
 				<Col xs={1} sm={6} md={6} lg={12} >
 					<footer class="py-5 bg-dark">
 						<div class="container">
-							<p class="m-0 text-center text-white">Copyright &copy; TodoBelleza.cl</p>
+							<p class="m-0 text-center text-white">Copyright &copy; prueba.cl</p>
 							<p class="m-0 text-center text-white">Contactar: +569 89194282</p>
 						</div>
 					</footer>
